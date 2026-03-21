29e63a253b035ece535f9308b18ddbba24baae30
Nicky <96009889+NickyEndK@users.noreply.github.com>
2026-03-21 16:22:37 +0100

    Add fifth star shape to stars.js

--- js/stars.js
+++ js/stars.js
@@ -3,4 +3,5 @@ export const STARS = [
     { id: 'star2', originalSize: 100, path: "M49,96L41,68,22,53,34,33l19,3L62,8,83,46,59,52,52,72Z" },
     { id: 'star3', originalSize: 100, path: "M46,92L29,53H48L38,31,62,21l2,22,5,10L55,68Z" },
     { id: 'star4', originalSize: 100, path: "M52,14L43,30,34,14s-7.045,21.182-6,25-16,2-16,2L32,56,42,74l9-22L65,66,63,43l11-8L53,31Z" }
+    { id: 'star5', originalSize: 100, path: "M50,5 L61,35 L95,35 L67,55 L78,85 L50,65 L22,85 L33,55 L5,35 L39,35 Z" }
 ];
