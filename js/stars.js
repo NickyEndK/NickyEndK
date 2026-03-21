6ba9e46a0bb8482928a40b0ab16d74bee026d2b9
Nicky <96009889+NickyEndK@users.noreply.github.com>
2026-03-21 16:14:28 +0100

    Add STARS constant with star SVG paths

---
+++ js/stars.js
@@ -0,0 +1,6 @@
+export const STARS = [
+    { id: 'star1', originalSize: 100, path: "M10,0 L13,7 L20,7 L14,11 L16,18 L10,14 L4,18 L6,11 L0,7 L7,7 Z" },
+    { id: 'star2', originalSize: 100, path: "M49,96L41,68,22,53,34,33l19,3L62,8,83,46,59,52,52,72Z" },
+    { id: 'star3', originalSize: 100, path: "M46,92L29,53H48L38,31,62,21l2,22,5,10L55,68Z" },
+    { id: 'star4', originalSize: 100, path: "M52,14L43,30,34,14s-7.045,21.182-6,25-16,2-16,2L32,56,42,74l9-22L65,66,63,43l11-8L53,31Z" }
+];
