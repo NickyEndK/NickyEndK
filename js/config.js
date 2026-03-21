5c044ec9adf1aadb7f2dcc6d9c26e729357582ca
Nicky <96009889+NickyEndK@users.noreply.github.com>
2026-03-21 16:15:09 +0100

    Remove STAR_DEFS from config.js

--- js/config.js
+++ js/config.js
@@ -6,11 +6,5 @@ export const CONFIG = {
     MAX_STAR_PLACEMENT_ATTEMPTS: 100,
     MIN_STAR_ALPHA: 0.2,
     MAX_STAR_ALPHA: 1.0,
-    LINE_DRAW_SPEED: 0.08,
-    STAR_DEFS: [
-        { id: '#star1', originalSize: 100 },
-        { id: '#star2', originalSize: 100 },
-        { id: '#star3', originalSize: 100 },
-        { id: '#star4', originalSize: 100 }
-    ]
+    LINE_DRAW_SPEED: 0.08
 };
